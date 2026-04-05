70795b9609cdee5d5d68b6100f816773a96ebc95
Codedoes <codedoes@gmail.com>
2026-04-05 20:04:39 +0200

    more clean up

--- swarm_test/run_swarm_test.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-pi -e /home/kit/dev/llm/pi-featherless-2/index.ts --model featherless-ai/zai-org/GLM-5 "Use swarm_scan on fileA.txt and fileB.txt. Confirm the main agent yields its concurrency slots to the swarm."
